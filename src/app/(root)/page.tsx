a2dbbfe15ebc0ba8ce7920380e0e0de0286c1c31
amh6281 <amh6281@naver.com>
2025-11-05 18:21:29 +0900

    feat: home page 추가

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,5 +1,53 @@
+import TradingViewWidget from '@/components/home/TradingViewWidget'
+import {
+    HEATMAP_WIDGET_CONFIG,
+    MARKET_DATA_WIDGET_CONFIG,
+    MARKET_OVERVIEW_WIDGET_CONFIG,
+    TOP_STORIES_WIDGET_CONFIG,
+} from '@/constants/widget'
+
 const Home = () => {
-    return <div className='home-wrapper flex min-h-screen'>Home</div>
+    const scriptUrl = `https://s3.tradingview.com/external-embedding/embed-widget-`
+
+    return (
+        <div className='home-wrapper flex min-h-screen'>
+            <section className='home-section grid w-full gap-8'>
+                <div className='md:col-span-1 xl:col-span-1'>
+                    <TradingViewWidget
+                        title='주요 종목 한눈에 보기'
+                        scriptUrl={`${scriptUrl}market-overview.js`}
+                        config={MARKET_OVERVIEW_WIDGET_CONFIG}
+                        className='custom-chart'
+                        height={600}
+                    />
+                </div>
+                <div className='md-col-span xl:col-span-2'>
+                    <TradingViewWidget
+                        title='주식 시장 히트맵'
+                        scriptUrl={`${scriptUrl}stock-heatmap.js`}
+                        config={HEATMAP_WIDGET_CONFIG}
+                        height={600}
+                    />
+                </div>
+            </section>
+            <section className='home-section grid w-full gap-8'>
+                <div className='h-full md:col-span-1 xl:col-span-1'>
+                    <TradingViewWidget
+                        scriptUrl={`${scriptUrl}timeline.js`}
+                        config={TOP_STORIES_WIDGET_CONFIG}
+                        height={600}
+                    />
+                </div>
+                <div className='h-full md:col-span-1 xl:col-span-2'>
+                    <TradingViewWidget
+                        scriptUrl={`${scriptUrl}market-quotes.js`}
+                        config={MARKET_DATA_WIDGET_CONFIG}
+                        height={600}
+                    />
+                </div>
+            </section>
+        </div>
+    )
 }
 
 export default Home
